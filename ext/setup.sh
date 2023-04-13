58786a068a33372319744818948b9507113bae0b
Paulo Cabral <sumolx@gmail.com>
2023-04-13 18:50:46 -0400

    dropbear will now build with main configuration

--- ext/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# dropbear
-rm -rf dropbear/build && mkdir dropbear/build && cd dropbear
-cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../toolchain/share/buildroot/toolchainfile.cmake -Bbuild
-cd build && make && cd ../../
