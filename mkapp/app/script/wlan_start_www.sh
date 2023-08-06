de57d7c3a8a89a4316da9b7f3d152a97385b377c
Paulo Cabral <sumolx@gmail.com>
2023-08-06 09:21:20 -0400

    hlsProxy output relocated to /tmp/www/live/

--- mkapp/app/script/wlan_start_www.sh
+++ mkapp/app/script/wlan_start_www.sh
@@ -7,17 +7,20 @@
     rm -rf /tmp/www
     cp -a /mnt/app/services/www /tmp/ && \
     cd /tmp/www && \
+    mkdir live && \
+    mkdir dvr && \
     ln -s /mnt/extsd/movies movies
 
     # Invoke Services
     if [ $? -eq 0 ]; then
-        /mnt/app/app/record/hlsProxy -T 8000 rtsp://127.0.0.1:8554/hdzero hdz_live > /tmp/hlsProxy.log 2>&1 &
         httpd -v -p 80 > /tmp/httpd.log 2>&1 &
+        cd live
+        /mnt/app/app/record/hlsProxy -T 8000 rtsp://127.0.0.1:8554/hdzero hdz > /tmp/hlsProxy.log 2>&1 &
     fi
 
     # Monitor Services
     sleep 5
-    while [ ! -z "$(ps | grep hlsProxy)" -a ! -z "$(ls /tmp/www/hdz_live.m3u8 2>/dev/null)" ]; do
+    while [ ! -z "$(ps | grep hlsProxy)" -a ! -z "$(ls /tmp/www/live/hdz.m3u8 2>/dev/null)" ]; do
         sleep 10
     done
     killall httpd
