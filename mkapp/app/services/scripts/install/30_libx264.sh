d2e457a293babcd64b33e746d2b3ce02d4f1dbb6
sumolx <sumolx@gmail.com>
2025-04-04 16:46:51 -0400

    Added ffmpeg with lib264 and hls support

---
+++ mkapp/app/services/scripts/install/30_libx264.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ln -sfn /mnt/app/services/libx264/lib/libx264.so.165 /lib/libx264.so.165
---
+++ mkapp/app/services/scripts/install/50_ffmpeg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -sfn /mnt/app/services/ffmpeg/lib/libavcodec.so.59 /lib/libavcodec.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavdevice.so.59 /lib/libavdevice.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavfilter.so.8 /lib/libavfilter.so.8
+ln -sfn /mnt/app/services/ffmpeg/lib/libavformat.so.59 /lib/libavformat.so.59
+ln -sfn /mnt/app/services/ffmpeg/lib/libavutil.so.57 /lib/libavutil.so.57
+ln -sfn /mnt/app/services/ffmpeg/lib/libpostproc.so.56 /lib/libpostproc.so.56
+
+ln -sfn /mnt/app/services/ffmpeg/bin/ffmpeg /bin/ffmpeg
+ln -sfn /mnt/app/services/ffmpeg/bin/ffprobe /bin/ffprobe
+
