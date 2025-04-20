631e59461f7ed305bfe660862102d4dd4f2cff7e
sumolx <sumolx@gmail.com>
2025-04-19 21:47:55 -0400

    Decreased startup delays

--- mkapp/app/script/wlan_start_www.sh
+++ mkapp/app/script/wlan_start_www.sh
@@ -6,5 +6,8 @@ rm -rf /tmp/www
 cp -a /mnt/app/services/www /tmp/
 ln -sfn /mnt/extsd/DCIM/100HDZRO /tmp/www/movies
 
-httpd -h /tmp/www
-curl -X POST "http://127.0.0.1/cgi-bin/live?play"
+# Non-blocking
+{
+    httpd -h /tmp/www
+    curl -X POST "http://127.0.0.1/cgi-bin/live?play"
+} &
