5791b545ef50da70b042e893f207ca6d0764f217
sumolx <sumolx@gmail.com>
2025-04-29 16:52:54 -0400

    Utilize SD Card as a cache for DVR playback.

--- mkapp/app/script/wlan_start_www.sh
+++ mkapp/app/script/wlan_start_www.sh
@@ -2,8 +2,19 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib:/lib/eyesee-mpp
 
+# Prepare web resources
 rm -rf /tmp/www
 cp -a /mnt/app/services/www /tmp/
+
+# Prepare dvr cache
+rm -rf /mnt/extsd/cache/www/dvr
+mkdir -p /mnt/extsd/cache/www
+cp -a /tmp/www/dvr /mnt/extsd/cache/www/
+rm -rf /tmp/www/dvr
+
+# Link sd card resources to web resources
+ln -sfn /mnt/extsd/cache/www/dvr /tmp/www/dvr
 ln -sfn /mnt/extsd/DCIM/100HDZRO /tmp/www/movies
 
+# Start server
 httpd -h /tmp/www
